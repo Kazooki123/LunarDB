ab697c372a7c825a4aa5f810b1f3f1112da076b0
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-10-19 09:32:56 +0800

    🐧changes on main.go

--- apis/api/v1/main.go
+++ apis/api/v1/main.go
@@ -1,18 +1,34 @@
 // Author: Kazooki123, StarloExoliz
 
-// Copyright 2024 Kazooki123
+/*
+** Copyright 2024 Kazooki123
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+** documentation files (the “Software”), to deal in the Software without restriction, including without 
+** limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies     
+** of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+** conditions:
+**
+** The above copyright notice and this permission notice shall be included in all copies or substantial 
+** portions of the Software.
+** 
+** THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+** LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+** THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+** 
+**/
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/*
+** 
+** This mimics on the functions of LunarDB
+** As executing the "lunar.exe" file itself is very tricky
+** So instead we code it in main.go 
+**
+**/ 
 
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
-// This mimics on the functions of LunarDB
-// As executing the "lunar.exe" file itself is very tricky
-// So instead code we it in main.go 
 
 package main
 
