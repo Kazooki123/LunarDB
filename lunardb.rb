28dacc44de71372aa69d23b894536e71cb5ea413
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-11-08 21:32:18 +0800

    Added lunardb.rb for the Homebrew formula and brew-publish.yml

---
+++ lunardb.rb
@@ -0,0 +1,20 @@
+class Lunar < Formula
+  desc "LunarDB: NoSQL, cache, in-memory key-value store database"
+  homepage "https://github.com/Kazooki123/LunarDB"
+  url "wget https://github.com/Kazooki123/LunarDB/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "{{SHA256}}"
+  license "MIT"
+
+  depends_on "lua"
+  depends_on "libpqxx"
+
+  def install
+    system "g++", "-std=c++17", "main.cpp", "cache.cpp", "saved.cpp", "sql.cpp", "module.cpp", "hashing.cpp",
+           "-I#{Formula["lua"].opt_include}/lua", "-llua", "-lpqxx", "-lpq", "-o", "lunar"
+    bin.install "lunar"
+  end
+
+  test do
+    system "#{bin}/lunar"
+  end
+end
